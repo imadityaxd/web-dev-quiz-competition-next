272ca4ae85b3dfae38c8bb9c01af17357250d79d
Ravish Bisht <ravishbisht85@gmail.com>
2024-07-18 12:54:44 +0530

    get user route add

---
+++ src/app/api/getuser/route.ts
@@ -0,0 +1,18 @@
+import { getTokenData } from "@/helpers/getTokenData/getData";
+import dbConnect from "@/lib/dbConnect";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  dbConnect();
+  try {
+    const userName = await getTokenData(request);
+
+    return NextResponse.json({
+      success: true,
+      message: "user found",
+      data: userName,
+    });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 400 });
+  }
+}
---
+++ src/helpers/getTokenData/getData.ts
@@ -0,0 +1,12 @@
+import jwt from "jsonwebtoken";
+import { NextRequest } from "next/server";
+
+export function getTokenData(request: NextRequest) {
+  try {
+    const token = request.cookies.get("token")?.value || "";
+    const decodedToken: any = jwt.verify(token, process.env.JWT_TOKEN_SECRET!);
+    return decodedToken.name;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+}
