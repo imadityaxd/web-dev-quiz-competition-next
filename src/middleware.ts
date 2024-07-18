1c53b20ebfa2659554174cdef4750459061d2599
Ravish Bisht <ravishbisht85@gmail.com>
2024-07-18 13:44:20 +0530

    middleware set

---
+++ src/middleware.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+  const token = request.cookies.get("token")?.value || "";
+  const isPublicPath =
+    path === "/about-competition" ||
+    path === "/start-quiz" ||
+    path === "/register";
+
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL("/play-quiz", request.nextUrl));
+  }
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL("/", request.nextUrl));
+  }
+}
+
+export const config = {
+  matcher: [
+    "/",
+    "/about-competition",
+    "/start-quiz",
+    "/register",
+    "/play-quiz",
+  ],
+};
