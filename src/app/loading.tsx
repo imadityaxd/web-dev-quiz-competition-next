e0243afbcfbd7ef8451f44064b2637f04c2c320e
Ravish Bisht <ravishbisht85@gmail.com>
2024-07-24 18:20:45 +0530

    404 and loading page ui made

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+"use client";
+import React from 'react';
+import { FaSpinner } from 'react-icons/fa';
 
-export default function loading() {
+const Loading: React.FC = () => {
   return (
-    <div>loading...</div>
-  )
-}
+    <div className="flex items-center justify-center min-h-screen bg-gray-900 text-white">
+      <div className="flex flex-col items-center">
+        <FaSpinner className="animate-spin text-6xl mb-4" />
+        <h1 className="text-xl font-semibold">Loading...</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+"use client";
+import Link from "next/link";
+import React from "react";
+import { FaExclamationTriangle } from "react-icons/fa"; // Optional icon
 
-export default function NotFound() {
+const NotFound: React.FC = () => {
   return (
-    <div>not-found</div>
-  )
-}
+    <div className="flex items-center justify-center min-h-screen bg-black-100 text-white p-4">
+      <div className="flex flex-col items-center justify-center text-center max-w-md mx-auto">
+        <FaExclamationTriangle className="text-6xl mb-4" />
+        <h1 className="text-4xl font-bold mb-2 text-purple md:text-5xl">
+          404 - Page Not Found
+        </h1>
+        <p className="text-lg mb-4 md:text-xl">
+          Sorry, the page you are looking for does not exist.
+        </p>
+        <Link
+          href="/"
+          className="text-blue-400 hover:underline text-lg md:text-xl"
+        >
+          Go Back to Home
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
