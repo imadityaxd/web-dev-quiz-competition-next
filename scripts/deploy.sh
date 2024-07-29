f12bcc22eaecb3a6b114506fb9c832e36cd00042
Ravish Bisht <ravishbisht85@gmail.com>
2024-07-29 19:05:13 +0530

    adding automation so that pushed changes will directly pushed to deployement server

---
+++ scripts/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+echo "Deployment started..."
+
+# Pull the latest version of the app
+git pull origin main
+echo "New changes copied to server !"
+
+echo "Installing Dependencies..."
+npm install --yes
+
+echo "Creating Production Build..."
+npm run build
+
+echo "PM2 Reload"
+pm2 reload 0
+
+echo "Deployment Finished!"
