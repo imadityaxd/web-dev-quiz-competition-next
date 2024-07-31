de3cf0a50754ba63593b3e6dc4e46cc063f9ca30
Ravish Bisht <ravishbisht85@gmail.com>
2024-07-31 07:02:45 +0530

    ui update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -33,8 +33,8 @@ export default function Hero() {
           </h2>
           <TextGenerateEffect
             className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl"
-            words="javaScript output based questions quizz competition starts on
-            everyday at 9:30pm."
+            words="javaScript output based questions quizz competition starts
+            everyday at 6:00 pm - 12:00 Pm (IST)."
           ></TextGenerateEffect>
           <Link href="/about-competition">
             <MagicButton
