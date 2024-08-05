202922c3cc364505dd795bac8b9962a9cb372945
Ravish Bisht <ravishbisht85@gmail.com>
2024-08-05 17:47:15 +0530

    ui improve

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,27 +4,20 @@ import Button from "./Button";
 
 export default function Hero() {
   return (
-    <div className="pb-4 md:pb-20 pt-32">
+    <div className="pt-32">
       <div></div>
-      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.1] bg-grid-black/[0.2]  flex items-center justify-center absolute top-0 left-0">
+      <div className="h-[50%] w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.1] bg-grid-black/[0.2]  flex items-center justify-center absolute top-0 left-0">
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
       </div>
 
       <div className="flex justify-center relative my-8 md:my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-          {/* <h3 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            web dev quiz platform
-          </h3> */}
           <h2 className="text-center text-[40px] md:text-5xl lg:text-6xl font-bold">
             Learn web development with{" "}
             <span className="text-purple">
               Quiz
             </span>
           </h2>
-          {/* <TextGenerateEffect
-            className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl"
-            words=""
-          ></TextGenerateEffect> */}
           <h3 className="uppercase tracking-widest text-xs text-center text-gray-300 pt-6 font-mono">
             Take part in javaScript output based Quiz and compete with other
             developers and see your rank on leaderboard.
