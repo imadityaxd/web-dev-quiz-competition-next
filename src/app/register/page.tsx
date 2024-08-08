0770e92af26f015e7f76ac06810e5e222722c1fd
imadityaxd <104629524+imadityaxd@users.noreply.github.com>
2024-08-08 13:28:26 +0530

    ui update

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -73,7 +73,7 @@ export default function Register() {
               </Label>
               <Input
                 id="firstname"
-                placeholder="Ravish"
+                placeholder="xdcoder"
                 type="text"
                 required
                 onChange={(e) => setName(e.target.value)}
@@ -85,7 +85,7 @@ export default function Register() {
               <Label htmlFor="firstname">Instagram Id &nbsp;(optional)</Label>
               <Input
                 id="insta"
-                placeholder="@cykoravish"
+                placeholder="@xdcoder.xyz"
                 type="text"
                 onChange={(e) => setInstaId(e.target.value)}
               />
